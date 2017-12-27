5c13bde73fd2228de3971f0badc044eb393c81f4
Saleh <mo.saleh@fetchr.us>
2017-12-27 23:16:25 +0400

    Save tweets in a file

--- src/archiver.py
+++ src/archiver.py
@@ -1,5 +1,8 @@
 """ Subscribe using pypubsub on config.TWEETS_CHANNEL for any new tweet, then save it in a file or DB!"""
+import src.config as config
+
+file = open(config.archiver_file_name, 'a')
 
 
 def new_tweet(tweet):
-    print(tweet._json)
+    print(tweet._json, file=file)
