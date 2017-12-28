a7018f2b5ea83430d2a8ead4311a4fd0b3814f0f
Saleh <mo.saleh@fetchr.us>
2017-12-28 10:04:12 +0400

    Fix archiver to store tweet as a json

--- src/archiver.py
+++ src/archiver.py
@@ -1,8 +1,10 @@
 """ Subscribe using pypubsub on config.TWEETS_CHANNEL for any new tweet, then save it in a file or DB!"""
 import src.config as config
+import json
 
 file = open(config.archiver_file_name, 'a')
 
 
 def new_tweet(tweet):
-    print(tweet._json, file=file)
+    tweet_json = json.dumps(tweet._json)
+    print(tweet_json, file=file)
